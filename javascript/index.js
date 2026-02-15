5095f006868157fce9f96c9c08a83e86814dcb31
programmermosharof <mosharof.dev@gmail.com>
2026-02-15 09:44:59 +0600

    3 topic is complete

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,19 @@
-const styles = document.getElementsByTagName('h1');
+//  option 2 click event handler
+ function makeYellow (){
+            document.body.style.backgroundColor = 'yellow'
+        }
+
+function makeGreen (){
+    document.body.style.backgroundColor = 'green'
+}
+
+const makBlue = document.getElementById('btn-blue');
+makBlue.onclick = function makBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+// option 3 get onclick event handler
+const btn = document.getElementById('color');
+btn.onclick= btnMake;
+function btnMake (){
+    document.body.style.backgroundColor = '	#ff00ff';
+}
